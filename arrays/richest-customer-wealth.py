04284d34ce12bb133d0fb0a29477823b84e5df48
okmd <md.softdeveloper@gmail.com>
2021-04-28 22:10:41 +0530

    richest-customer-wealth

---
+++ arrays/richest-customer-wealth.py
@@ -0,0 +1,8 @@
+def maximumWealth(self, accounts: List[List[int]]) -> int:
+        max_sum=0
+        for row in range(len(accounts)):
+            row_sum=0
+            for col in range(len(accounts[0])):
+                row_sum += accounts[row][col]
+            max_sum = max(max_sum, row_sum)
+        return max_sum
