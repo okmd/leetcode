f9011194df4fa4d9fe90af3f694db900a26ed591
okmd <md.softdeveloper@gmail.com>
2021-05-17 21:53:25 +0530

    added block swap

--- 450Q/cyclically-rotate-an-array-by-one2614.py
+++ 450Q/cyclically-rotate-an-array-by-one2614.py
@@ -1,6 +1,12 @@
 # https://practice.geeksforgeeks.org/problems/cyclically-rotate-an-array-by-one2614/1
 # https://www.geeksforgeeks.org/array-rotation/
 import array
+# Different types of algorithm for array rotation.
+## One by one rotaion.
+## Juggling algorithm.
+## Reversal algorithm
+## Block swap algorithm
+
 '''
 Method 1: Rotation by temp array. time=O(n), space=O(d)
     -> store first d elements in the temp array.
@@ -70,10 +76,8 @@ show(method2(myarr, n), "Method 2")
 
 '''
 Method 3: Juggling algorithm extension of method 2.
-    -> rotate gcd(n, d) times at one time instead of only one rotation
-    -> make temp array of g size.
-    -> rotate one gcd size array at one time, till all gets rotated.
-    -> place temp at the end.
+    -> divide the array into d size array of n/gcd.
+    -> 
 '''
 
 
@@ -83,9 +87,10 @@ def method3(arr, d):
         temp = arr[i]
         j = i
         while True:
-            k = j + d
-            if k >= len(arr):
-                k = k - len(arr)
+            # k = j + d
+            # if k >= len(arr):
+            #     k = k - len(arr)
+            k = (j+d)%len(arr)
             if k == i:
                 break
             arr[j] = arr[k]
@@ -99,7 +104,7 @@ show(method3(myarr, n), "Method 3")
 
 
 '''
-Method 4: Reverse 2 times to get the original one.
+Method 4: Reversal algo. Reverse 2 times to get the original one.
     -> reverse d items, reverse remaining items.
     -> reverse the complete list.
 '''
@@ -123,3 +128,62 @@ def method4(arr, d):
 
 myarr = array.array('i', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
 show(method4(myarr, n), "Method 4")
+
+'''
+Method 4: block swap for array rotation
+    -> create 2 array A,B of size 0 to d-1 and d to n-1.
+    -> if A is small, spilt B into Bl, Br and |A| = |Br|.
+    -> swap A and Br in ABlBr->BrBlA
+    -> recur for remaining Bl
+    -> similarly for A if larger and |Al| = |B|.
+    -> when both are equal swap them A, B
+    
+'''
+# Recursive
+
+
+def method5(arr, i, d, n):
+    # none or all element to rotate.
+    if d == 0 or d == n:
+        return
+    # only two sets of size d.
+    if n-d == d:
+        # block swap
+        swap(arr, i, n - d+i, d)
+        return
+    if d < n-d:
+        swap(arr, i, n - d + i, d)
+        method5(arr, i, d, n - d)
+    else:
+        swap(arr, i, d, n - d)
+        method5(arr, n - d + i, 2*d - n, d)
+
+
+def swap(arr, fi, si, d):
+    for i in range(d):
+        arr[fi+i], arr[si+i] = arr[si+i], arr[fi+i]
+
+
+myarr = array.array('i', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+method5(myarr, 0, n, len(myarr))
+show(myarr, "Method 5")
+
+
+def method5_itr(arr, d, n):
+    if d == 0 or d == n:
+        return
+    i, j = d, n-d
+    while i != j:
+        if i < j:  # A is shorter
+            swap(arr, d-i, d+j-i, i)
+            j -= i
+        else:  # B is shorter
+            swap(arr, d-i, d, j)
+            i -= j
+    # swap all elements from d-i onwards with d onwards, j or i elements.
+    swap(arr, d-i, d, j)
+
+
+myarr = array.array('i', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+method5_itr(myarr, n, len(myarr))
+show(myarr, "Method 5 itr")
