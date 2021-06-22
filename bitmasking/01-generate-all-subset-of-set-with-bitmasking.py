8c76ce05099202af8f355d60f7393e158b9f987e
okmd <md.softdeveloper@gmail.com>
2021-06-22 10:04:45 +0530

    updated bitmasking

--- bitmasking/01-generate-all-subset-of-set-with-bitmasking.py
+++ bitmasking/01-generate-all-subset-of-set-with-bitmasking.py
@@ -15,9 +15,31 @@ def find_subsets(elements):
         subsets.append("".join(subset))
     return subsets
 
+def find_subsets1(elements):
+    n = len(elements)
+    subsets = []
+    for mask in range(0, 1<<n):
+        subset = []
+        j = 0
+        while mask:
+            # odd i.e last bit is 1
+            if mask&1:
+                # add only the bit which is set.
+                subset.append(elements[j])
+            j+=1
+            mask = mask>>1
+        subsets.append("".join(subset))
+    return subsets
+
 
 # printing the code
 elements = "ABC"
 subsets = find_subsets(elements)
-for i,subset in enumerate(subsets):
-    print(f"{i+1}. {subset}")
+subsets1 = find_subsets1(elements)
+
+def main(subsets):
+    for i,subset in enumerate(subsets):
+        print(f"{i+1}. {subset}")
+    
+main(subsets)
+main(subsets1)
