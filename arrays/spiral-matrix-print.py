cec79184539a41ea64e1f0266eafe20e86981e49
okmd <md.softdeveloper@gmail.com>
2021-07-23 16:37:49 +0530

    spiral print of matrix recursive

--- arrays/spiral-matrix-print.py
+++ arrays/spiral-matrix-print.py
@@ -47,10 +47,40 @@ def spiral_print(mat, r, c):
     return " ".join(map(str, output))
 
 
+def is_valid(top, bottom, left, right):
+    return left <= right and top <= bottom
+
+
+def spiral_recursive(mat, top, bottom, left, right, output):
+    if not (left <= right and top <= bottom):
+        return " ".join(map(str, output))
+    
+    for i in range(left, right+1):
+        output.append(mat[top][i])
+    top += 1
+    
+    if left <= right and top <= bottom:
+        for i in range(top, bottom+1):
+            output.append(mat[i][right])
+        right -= 1
+    
+    if left <= right and top <= bottom:
+        for i in range(right, left-1, -1):
+            output.append(mat[bottom][i])
+        bottom -= 1
+    
+    if left <= right and top <= bottom:
+        for i in range(bottom, top-1, -1):
+            output.append(mat[i][left])
+        left += 1
+
+    return spiral_recursive(mat, top, bottom, left, right, output)
+
+
 class TestCode(unittest.TestCase):
     def setUp(self):
         pass
-    
+
     def get_r_c(self, mat):
         if len(mat):
             return len(mat), len(mat[0])
@@ -63,6 +93,8 @@ class TestCode(unittest.TestCase):
                   [0, 4, 4]]
         r, c = self.get_r_c(matrix)
         self.assertEqual(spiral_print(matrix, r, c), "1 2 3 6 9 4 4 0 7 4 5 8")
+        self.assertEqual(spiral_recursive(matrix, 0, r-1, 0,
+                         c-1, []), "1 2 3 6 9 4 4 0 7 4 5 8")
 
     def test_spiral_uniform(self):
         matrix = [[1, 2, 3],
@@ -70,11 +102,14 @@ class TestCode(unittest.TestCase):
                   [7, 8, 9]]
         r, c = self.get_r_c(matrix)
         self.assertEqual(spiral_print(matrix, r, c), "1 2 3 6 9 8 7 4 5")
+        self.assertEqual(spiral_recursive(
+            matrix, 0, r-1, 0, c-1, []), "1 2 3 6 9 8 7 4 5")
 
     def test_spiral_row(self):
         matrix = [[1, 2, 3]]
         r, c = self.get_r_c(matrix)
         self.assertEqual(spiral_print(matrix, r, c), "1 2 3")
+        self.assertEqual(spiral_recursive(matrix, 0, r-1, 0, c-1, []), "1 2 3")
 
     def test_spiral_col(self):
         matrix = [[1],
@@ -82,12 +117,13 @@ class TestCode(unittest.TestCase):
                   [7]]
         r, c = self.get_r_c(matrix)
         self.assertEqual(spiral_print(matrix, r, c), "1 4 7")
-        
+        self.assertEqual(spiral_recursive(matrix, 0, r-1, 0, c-1, []), "1 4 7")
+
     def test_spiral_empty(self):
         matrix = []
         r, c = self.get_r_c(matrix)
         self.assertEqual(spiral_print(matrix, r, c), "")
-    
+        self.assertEqual(spiral_recursive(matrix, 0, r-1, 0, c-1, []), "")
 
 
 if __name__ == "__main__":
