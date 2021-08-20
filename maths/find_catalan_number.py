5891fc60bdd6d80026242320648d1bae01e153a9
okmd <md.softdeveloper@gmail.com>
2021-08-20 11:30:08 +0530

    added catalan permutation

--- maths/find_catalan_number.py
+++ maths/find_catalan_number.py
@@ -16,16 +16,52 @@ def Cn(n):
     dp[n] = ((4*n-2) / (n+1))*Cn(n-1)
     return dp[n]
 
+
+def gcd(a, b):
+    if a == 0:
+        return b
+    return gcd(b % a, a)
+
+
+def Cn_fact(n):
+    # 2nCn / (n+1)
+    # 2n! / (n+1)! (n)!
+    # (2n)! / (n+1) n! n!
+    # {(2n)*(2n-1)*(2n-2)* .. *(2n-(n-1))*(2n-n)!}\ {n*(n-1)*(n-2)* .. *1}
+    temp = n
+    num = 1
+    den = 1
+    r = n
+    n = 2*n
+    if r!=1:
+        while r:
+            num *= n
+            den *= r
+            g = gcd(num, den)
+            num //= g
+            den //= g
+            n = n-1
+            r = r-1
+    else:
+        num = n
+    num = num//(temp+1)
+    return num
+
+
 class Test(unittest.TestCase):
     def test_1(self):
         self.assertEqual(Cn(6), 132)
+        self.assertEqual(Cn_fact(6), 132)
         print(dp)
         self.assertEqual(Cn(5), 42)
         self.assertEqual(Cn(4), 14)
+        self.assertEqual(Cn_fact(4), 14)
         self.assertEqual(Cn(3), 5)
         self.assertEqual(Cn(2), 2)
         print(dp)
         self.assertEqual(Cn(1), 1)
+        self.assertEqual(Cn_fact(1), 1)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     unittest.main()
