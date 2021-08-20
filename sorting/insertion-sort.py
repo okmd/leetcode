b39e61a88c01fa3e6860878d8193dd117b96e5c2
okmd <md.softdeveloper@gmail.com>
2021-08-20 11:48:47 +0530

    added to sort complete array

--- sorting/insertion-sort.py
+++ sorting/insertion-sort.py
@@ -19,6 +19,18 @@ def find_median(arr, ele):
         return arr[n//2]
     return (arr[n//2 - 1] + arr[(n//2)])/2
 
+def insertion_sort_complete(arr):
+    i = 1
+    while i<len(arr):
+        j = i-1
+        key = arr[i]
+        while j>=0 and arr[j] > key:
+            arr[j+1] = arr[j]
+            j-=1
+        arr[j+1] = key
+        i+=1
+
+
 
 if __name__ == "__main__":
     arr = []
@@ -34,3 +46,7 @@ if __name__ == "__main__":
     for i in elements:
         print(f"Median at {i}: ", find_median(arr, i))
 
+    # sorting when elements are present
+    print("Before", elements)
+    insertion_sort_complete(elements)
+    print(elements)
