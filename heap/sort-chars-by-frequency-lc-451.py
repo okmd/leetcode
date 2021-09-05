39655111ed523034e638eb663e8073a4f5f1195d
okmd <md.softdeveloper@gmail.com>
2021-09-05 15:57:52 +0530

    sort-char-by-req

---
+++ heap/sort-chars-by-frequency-lc-451.py
@@ -0,0 +1,28 @@
+# Given string and you need to sort according to the frequency of chars.
+# The idea is to maintain a max heap
+# sort max heap then return
+import heapq
+def sort_frequently(string):
+    count = {}
+    for i in string:
+        if not count.get(i, None):
+            count[i] = 0
+        count[i] += 1
+    
+    # convert to list to heapify based on count
+    heap = [(c,v) for (v,c) in count.items()]
+    print(heap)
+    heapq._heapify_max(heap)
+    ans = []
+    while len(heap):
+        c, val = heapq._heappop_max(heap)
+        while c:
+            c-=1
+            ans.append(val)
+    return "".join(ans)
+
+
+
+
+string = "Aabb"
+print(sort_frequently(string))
