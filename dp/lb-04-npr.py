8704239600ee4b582000ba9b99d632dc80e96e40
okmd <md.softdeveloper@gmail.com>
2021-12-25 11:59:57 +0530

    permutation npr dp

--- dp/lb-04-npr.py
+++ dp/lb-04-npr.py
@@ -1,3 +1,6 @@
+# nPr = (n-1)Pr + r*(n-1)P(r-1)
+# nCr = nPr/r!
+
 def nPr(n, r):
     # O(n) and O(1)
     # n*(n-1)*..(n-r)!/(n-r)!
@@ -16,8 +19,19 @@ def dp_fact(n, r):
         fact[i] = i*fact[i-1]
     return fact[n]/fact[n-r]
 
+def tabulation(n,r):
+    dp =[[0]*(r+1) for _ in range(n+1)]
+    for i in range(n+1):
+        for j in range(r+1):
+            if i==0 and j==0:
+                dp[i][j] = 1
+            else:
+                dp[i][j] = dp[i-1][j] + j*dp[i-1][j-1]
+    return dp[n][r]
+
 
 n = 3
-r = 3
+r = 2
 print(nPr(n, r))
 print(dp_fact(n, r))
+print(tabulation(n, r))
