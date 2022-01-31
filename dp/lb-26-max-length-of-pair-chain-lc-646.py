bd83cbd715b8349427e87607790870e0878c3c54
okmd <md.softdeveloper@gmail.com>
2022-01-31 10:55:59 +0530

    added optimized code nlogn

--- dp/lb-26-max-length-of-pair-chain-lc-646.py
+++ dp/lb-26-max-length-of-pair-chain-lc-646.py
@@ -2,7 +2,10 @@
 # two pair p1(a,b), p2(c,d) form chain if b<c
 # find the max chain length
 
-# using lis technique
+# using lis technique O(n^2)
+from ast import operator
+
+
 def max_chain_len(arr):
     # sort the array based on right value
     n = len(arr)
@@ -21,9 +24,22 @@ def max_chain_len(arr):
         i+=1
     return max(dp)# max element will be the chain length for complete array
     
- 
+# optimization over time: O(nlong)
+def optimization(arr):
+    arr.sort(key=lambda x:x[1])
+    curr, ans = float('-inf'), 0
+    for st, ed in arr:
+        if curr<st:
+            curr = ed 
+            ans+=1
+    return ans
+# as the chain will always be in the form of 
+# a<b<c<d ... 
+     
  
     
 # RUN: CODE DP with GRID  
 print(max_chain_len([[1,2],[3,4],[2,3]])) #2
 print(max_chain_len([[7,9],[4,5],[7,9],[-7,-1],[0,10],[3,10],[3,6],[2,3]])) # 4
+print(optimization([[1,2],[3,4],[2,3]])) #2
+print(optimization([[7,9],[4,5],[7,9],[-7,-1],[0,10],[3,10],[3,6],[2,3]])) # 4
