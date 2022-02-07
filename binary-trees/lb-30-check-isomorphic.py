d627dd7f3e70718db4310cd0267469f0dd3957e3
okmd <md.softdeveloper@gmail.com>
2022-02-07 13:13:34 +0530

    check-isomorphism

--- binary-trees/lb-30-check-isomorphic.py
+++ binary-trees/lb-30-check-isomorphic.py
@@ -3,21 +3,21 @@
 # isomorphic if child can be swapped at any level
 
 def isomorphic(root1, root2):
-            # if both none then true
-            if not (root1 or root2):
-                return True
-            # it is about both trees not left and right child of same tree
-            # if either of not none
-            if not (root1 and root2):
-                return False
-            if root1.data != root2.data:
-                return False
-            # swapped  
-            # ie. if left become right then right should become left and both should be isomorphic
-            swapped = isomorphic(root1.right, root2.left) and  isomorphic(root1.left, root2.right)
-        
-            # not swapped
-            # ie. if left is left then right sould be right and both should be isomorphic
-            not_swapped = isomorphic(root1.left, root2.left) and  isomorphic(root1.right, root2.right)
-            
-            return swapped or not_swapped
+    # if both none then true
+    if not (root1 or root2):
+        return True
+    # it is about both trees not left and right child of same tree
+    # if either of not none
+    if not (root1 and root2):
+        return False
+    if root1.data != root2.data:
+        return False
+    # swapped  
+    # ie. if left become right then right should become left and both should be isomorphic
+    swapped = isomorphic(root1.right, root2.left) and  isomorphic(root1.left, root2.right)
+
+    # not swapped
+    # ie. if left is left then right sould be right and both should be isomorphic
+    not_swapped = isomorphic(root1.left, root2.left) and  isomorphic(root1.right, root2.right)
+    
+    return swapped or not_swapped
