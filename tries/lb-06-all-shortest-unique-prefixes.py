ff22e64599f7e2ea1bc38ed81f348a3d6c9e5f6e
okmd <md.softdeveloper@gmail.com>
2022-02-11 10:56:21 +0530

    gives TLE

--- tries/lb-06-all-shortest-unique-prefixes.py
+++ tries/lb-06-all-shortest-unique-prefixes.py
@@ -1,6 +1,7 @@
 class TrieNode:
     def __init__(self):
-        self.chars = [[0,0]]*26 # Node and count of words starting with this node
+        self.freq = 0 # denotes the word count starting from here
+        self.chars = [0]*26 # Node and count of words starting with this node
         self.terminal = False
 
 
@@ -21,11 +22,10 @@ class Trie:
         root = self.root
         for w in word:
             index = self.get_idx(w)
-            if not root.chars[index][0]:
-                root.chars[index] = [TrieNode(), 0]
-            # already present then increase count and move ahead
-            root.chars[index][1]+=1
-            root = root.chars[index][0]
+            if not root.chars[index]:
+                root.chars[index] = TrieNode()
+            root = root.chars[index]
+            root.freq += 1
         root.terminal = True
     
     def unique_prefixes(self, words):
@@ -33,29 +33,30 @@ class Trie:
         def helper(root, word):
             for i, w in enumerate(word):
                 index = self.get_idx(w)
-                if root.chars[index][0]:
-                    # only one word from this node then it must be the prefix
-                    if root.chars[index][1]==1:
+                if root.chars[index]:
+                    if root.chars[index].freq==1:
+                        # prefix answer
                         ans.append(word[:i+1])
                         break
-                    root = root.chars[index][0]
-                # else not present in the tree
+                    root = root.chars[index]
 
-          
-        for w in words:          
-            helper(self.root, w)
-            
+        for word in words:      
+            helper(self.root, word) 
         return ans
                 
     
 t = Trie()
-# c = ["zebra", "dog", "duck", "dove"]
-c = ["geeksgeeks", "geeksquiz", "geeksforgeeks"]
-for i in c:
-    t.insert(i)
-print(t.unique_prefixes(c))
+words = ["zebra", "dog", "duck", "dove"]
+words = ["geeksgeeks", "geeksquiz", "geeksforgeeks"]
+
+for word in words:
+    t.insert(word)
+
+print(t.unique_prefixes(words))
+# print(ans)
 
 # Note Trie can also be modified to store the count of each word staring with char
 # to reduce the constant searching time each time for each node = O(nk)
 
 # Giving Memory Error on Large input?
+# Giving TLE even after using frequency per node instead of char.
