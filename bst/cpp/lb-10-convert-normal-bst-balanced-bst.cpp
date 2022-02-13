9017cf44f0ffd9827e603084dd1c4b58271717bf
okmd <md.softdeveloper@gmail.com>
2022-02-13 16:17:25 +0530

    convert normal bst to balanced bst

---
+++ bst/cpp/lb-10-convert-normal-bst-balanced-bst.cpp
@@ -0,0 +1,24 @@
+void find_inorder(Node *root, vector<int> &ino) {
+    if (!root) return;
+    find_inorder(root->left, ino);
+    ino.push_back(root->data);
+    find_inorder(root->right, ino);
+}
+Node *helper(vector<int> &arr, int start, int end) {
+    if (start > end) return NULL;
+    int mid = start + int((end - start) / 2);
+    Node *root = new Node(arr[mid]);
+    root->left = helper(arr, start, mid - 1);
+    root->right = helper(arr, mid + 1, end);
+    return root;
+}
+Node *convert_balanced_bst(Node *root) {
+    // # find the inorder traversal of tree.
+    // # divide in the middle of inorder traversals.
+    // # now create left, right child form sub tree.
+    vector<int> arr;
+    find_inorder(root, arr);
+    sort(arr.begin(), arr.end());
+
+    return helper(root, 0, arr.size() - 1);
+}
---
+++ bst/lb-10-convert-normal-bst-balanced-bst.py
@@ -0,0 +1,30 @@
+# Balanced bst is the bst where height difference is atmost 1.
+def find_inorder(root):
+    arr = []
+
+    def find(root):
+        if not root:
+            return
+        find(root.left)
+        arr.append(root.data)
+        find(root.right)
+    find(root)
+    return arr
+
+
+def convert_balanced_bst(root):
+    # find the inorder traversal of tree.
+    # divide in the middle of inorder traversals.
+    # now create left, right child form sub tree.
+    ino = find_inorder(root)
+    ino = sorted(ino)
+
+    def helper(arr, start, end):
+        if start > end:
+            return None
+        mid = start + int((end-start)//2)
+        root = Node(arr[mid])
+        root.left = helper(arr, start, mid-1)
+        root.right = helper(arr, mid+1, end)
+        return root
+    return helper(root, 0, len(ino)-1)
