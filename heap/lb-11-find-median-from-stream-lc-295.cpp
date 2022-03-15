1d8bba326b6775046076d3da49419790d1e91b96
okmd <md.softdeveloper@gmail.com>
2022-03-15 18:54:49 +0530

    msg,

--- heap/lb-11-find-median-from-stream-lc-295.cpp
+++ heap/lb-11-find-median-from-stream-lc-295.cpp
@@ -79,7 +79,7 @@ class Solution {
         int mns = min_heap.size();
         int mxs = max_heap.size();
         // if odd then top of max heap is median
-        // if even then mean of top of min and max heap
+        // if even then mean of top of min and max heap.
         return (mxs > mns) ? double(max_heap.top()) : double((max_heap.top() + min_heap.top()) / 2);
     }
 };
