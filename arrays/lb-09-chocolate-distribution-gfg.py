b77fadfd01a63583cfe086eb8f4d19191f6c8d13
okmd <md.softdeveloper@gmail.com>
2022-03-19 20:30:59 +0530

    chocolate distribution

---
+++ arrays/lb-09-chocolate-distribution-gfg.py
@@ -0,0 +1,13 @@
+def findMinDiff(self, A, N, M):
+    if N < M:
+        return 0
+    if N == 0 or M == 0:
+        return 0
+
+    A.sort()
+    i = 0
+    mx = sys.maxsize
+    while i+M <= N:
+        mx = min(mx, A[i+M-1] - A[i])
+        i += 1
+    return mx
