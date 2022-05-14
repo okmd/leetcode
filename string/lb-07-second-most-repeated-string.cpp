09c346585390aabe7017661c83b57ca71f33b631
okmd <md.softdeveloper@gmail.com>
2022-05-14 22:57:12 +0530

    second most repeated string in a list of strings

---
+++ string/lb-07-second-most-repeated-string.cpp
@@ -0,0 +1,19 @@
+string secFrequent (string arr[], int n)
+    {
+        unordered_map<string, int> mp;
+        for(int i=0; i<n; i++){
+            if(mp.find(arr[i]) == mp.end()){
+                mp[arr[i]] = 0;
+            }
+            mp[arr[i]]++;
+            
+        }
+        auto ans = max_element(mp.begin(), mp.end(), [](auto x, auto y){
+            return x.second < y.second;
+        });
+        mp.erase(ans->first);
+        ans = max_element(mp.begin(), mp.end(), [](auto x, auto y){
+            return x.second < y.second;
+        });
+        return ans->first;
+    }
