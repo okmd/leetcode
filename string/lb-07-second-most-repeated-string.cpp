cd1128f9977615470e4c64ba4937b6ccc11a99da
okmd <md.softdeveloper@gmail.com>
2022-05-15 12:50:07 +0530

    second most repeated string

--- string/lb-07-second-most-repeated-string.cpp
+++ string/lb-07-second-most-repeated-string.cpp
@@ -1,19 +1,17 @@
-string secFrequent (string arr[], int n)
-    {
-        unordered_map<string, int> mp;
-        for(int i=0; i<n; i++){
-            if(mp.find(arr[i]) == mp.end()){
-                mp[arr[i]] = 0;
-            }
-            mp[arr[i]]++;
-            
+string secFrequent(string arr[], int n) {
+    unordered_map<string, int> mp;
+    for (int i = 0; i < n; i++) {
+        if (mp.find(arr[i]) == mp.end()) {
+            mp[arr[i]] = 0;
         }
-        auto ans = max_element(mp.begin(), mp.end(), [](auto x, auto y){
-            return x.second < y.second;
-        });
-        mp.erase(ans->first);
-        ans = max_element(mp.begin(), mp.end(), [](auto x, auto y){
-            return x.second < y.second;
-        });
-        return ans->first;
+        mp[arr[i]]++;
     }
+    auto ans = max_element(mp.begin(), mp.end(), [](auto x, auto y) {
+        return x.second < y.second;
+    });
+    mp.erase(ans->first);
+    ans = max_element(mp.begin(), mp.end(), [](auto x, auto y) {
+        return x.second < y.second;
+    });
+    return ans->first;
+}
