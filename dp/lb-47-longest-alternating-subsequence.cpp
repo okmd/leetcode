07eb7964a068f6d5717e4bd980b228b2508a05f3
okmd <md.softdeveloper@gmail.com>
2022-05-18 09:34:52 +0530

    longest alternating subsequences

--- dp/lb-47-longest-alternating-subsequence.cpp
+++ dp/lb-47-longest-alternating-subsequence.cpp
@@ -5,11 +5,11 @@ int AlternatingaMaxLength(vector<int>& nums) {
     vector<vector<int>> dp(n, vector<int>(2, 1));  // prev less, prev more
     for (int i = 1; i < n; i++) {
         for (int j = 0; j < i; j++) {
-            if (nums[i] > nums[j]) // all subsequences till j@decreasing + 1
+            if (nums[i] > nums[j])  // all subsequences till j@decreasing + 1
                 dp[i][0] = max(1 + dp[j][1], dp[i][0]);
             else if (nums[i] < nums[j])  // not valid of same values
                 dp[i][1] = max(1 + dp[j][0], dp[i][1]);
-                // all subsequences till j@increasing + 1
+            // all subsequences till j@increasing + 1
         }
         mx = max({mx, dp[i][0], dp[i][1]});
     }
