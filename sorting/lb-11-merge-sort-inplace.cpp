cdb969a1b4edeb040e7246485ff7ade745f04464
okmd <md.softdeveloper@gmail.com>
2022-05-22 22:31:41 +0530

    merge sort inplace

---
+++ sorting/lb-11-merge-sort-inplace.cpp
@@ -0,0 +1,42 @@
+// n^2
+void merge(int arr[], int l, int m, int r) {
+    int i = l;
+    int j = m + 1;
+    // one time check for already sorted
+    if (arr[j] > arr[m]) return;
+    while (i <= m and j <= r) {
+        if (arr[i] > arr[j]) {
+            swap(arr[i], arr[j]);
+            int k = j + 1;
+            int temp = arr[j];
+            while (k <= r and arr[k] < temp) {
+                arr[k - 1] = arr[k];
+                k++;
+            }
+            arr[k - 1] = temp;
+        }
+        i++;
+    }
+}
+void mergeSort(int arr[], int l, int r) {
+    if (l >= r) return;
+    int mid = l + (r - l) / 2;
+    mergeSort(arr, l, mid);
+    mergeSort(arr, mid + 1, r);
+    merge(arr, l, mid, r);
+}
+
+// Merge using gap method
+// logn*n
+void merge(int arr[], int l, int m, int r) {
+    int gap = (r - l + 1);
+    while (gap > 1) {
+        gap = ceil(gap / 2.0);
+        for (int i = l; i + gap <= r; i++) {
+            int j = i + gap;
+            if (arr[i] > arr[j])
+                swap(arr[i], arr[j]);
+        }
+    }
+}
+// Intuition?Why?
