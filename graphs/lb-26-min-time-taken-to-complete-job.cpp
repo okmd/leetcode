96ad07ea8cf0ee1441b8ca81b5e382e2f31876ae
okmd <md.softdeveloper@gmail.com>
2022-07-02 17:53:11 +0530

    min time taken to complete job

--- graphs/lb-26-min-time-taken-to-complete-job.cpp
+++ graphs/lb-26-min-time-taken-to-complete-job.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include<vector>
 #include<stack>
+#include<queue>
 using namespace std;
 class Graph {
     int V;
@@ -11,31 +12,60 @@ public:
         adj = new vector<int>[V];
     }
     void addEdge(int u, int v) {
-        adj[u-1].push_back(v-1);
+        adj[u].push_back(v);
     }
-    void _dfs(int i, int level, vector<int>& visited, stack<pair<int,int>>& st) {
+    void _dfs(int i, int level, vector<int>& visited, stack<pair<int, int>>& st) {
         visited[i] = true;
         for (auto neb : adj[i]) {
             if (!visited[neb]) {
-                _dfs(neb, level+1, visited, st);
+                _dfs(neb, level + 1, visited, st);
             }
         }
-        st.push({i+1, level});
+        st.push({ i, level });
     }
-    void printOrder() {
-        stack<pair<int,int>> st;
+    void printOrder(vector<int>& ans) {
+        stack<pair<int, int>> st;
         vector<int> visited(V, false);
         for (int i = 0; i < V; i++) {
             if (!visited[i]) {
                 _dfs(i, 1, visited, st);
             }
         }
-        while(st.size()){
-            cout<<"("<<st.top().first<<", "<<st.top().second<<") ";
+        while (st.size()) {
+            cout << "(" << st.top().first << ", " << st.top().second << ") ";
+            ans[st.top().first]=st.top().second;
             st.pop();
         }
     }
-    ~Graph(){
+    void printOrder1(vector<int>& ans) {
+        vector<int> indegree(V, 0);
+        for (int i = 0; i < V; i++) {
+            for (auto v : adj[i])
+                indegree[v]++;
+        }
+
+        queue<int> q;
+        // push all elements with indegree 0
+        for (int i = 0; i < V; i++)
+            if (indegree[i] == 0) q.push(i);
+        int count = 0;
+        while (q.size()) {
+            int m = q.size();
+            count++;
+            while (m--) {
+                int p = q.front();
+                q.pop();
+                ans[p] = count;
+                // remove adjacent indegree
+                for (auto id : adj[p]) {
+                    indegree[id]--;
+                    if (!indegree[id]) q.push(id);
+                }
+            }
+
+        }
+    }
+    ~Graph() {
         delete adj;
     }
 };
@@ -45,21 +75,29 @@ int main() {
     Graph g = Graph(n);
 
     // Given Directed Edges of graph
-    g.addEdge(1, 3);
-    g.addEdge(1, 4);
-    g.addEdge(1, 5);
-    g.addEdge(2, 3);
-    g.addEdge(2, 8);
-    g.addEdge(2, 9);
-    g.addEdge(3, 6);
-    g.addEdge(4, 6);
-    g.addEdge(4, 8);
-    g.addEdge(5, 8);
-    g.addEdge(6, 7);
-    g.addEdge(7, 8);
-    g.addEdge(8, 10);
+    g.addEdge(1-1, 3-1);
+    g.addEdge(1-1, 4-1);
+    g.addEdge(1-1, 5-1);
+    g.addEdge(2-1, 3-1);
+    g.addEdge(2-1, 8-1);
+    g.addEdge(2-1, 9-1);
+    g.addEdge(3-1, 6-1);
+    g.addEdge(4-1, 6-1);
+    g.addEdge(4-1, 8-1);
+    g.addEdge(5-1, 8-1);
+    g.addEdge(6-1, 7-1);
+    g.addEdge(7-1, 8-1);
+    g.addEdge(8-1, 10-1);
 
     // Function Call
-    g.printOrder();
+    vector<int>ans(n);
+    g.printOrder(ans);
+    for (auto s : ans) {
+        cout << s << " ";
+    }
+    g.printOrder1(ans);
+    for (auto s : ans) {
+        cout << s << " ";
+    }
     return 0;
 }
